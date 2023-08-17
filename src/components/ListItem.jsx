5b75567e621e240edb30529ebb8634c90e8f888d
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2023-08-17 14:27:30 -0700

    Make requested changes per PR code review

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,19 +2,4 @@ import './ListItem.css';
 
 export function ListItem({ name }) {
 	return <li className="ListItem">{name}</li>;
-	// <ul>
-	// 	{
-	// 		name.map((item) => (
-	// 			<ListItem name={item.name} />
-
-	// 		))}
-	// </ul>
 }
-
-/*
-
-Fetch Data from the Database:
-
-Render the Data:
-
-**/
--- src/views/List.jsx
+++ src/views/List.jsx
@@ -4,17 +4,11 @@ export function List({ data }) {
 	return (
 		<>
 			<p>
-				Hello from the <code>/list</code> page! Our first test
+				Hello from the <code>/list</code> page!
 			</p>
 			<ul>
-				{/**
-				 * TODO: write some JavaScript that renders the `data` array
-				 * using the `ListItem` component that's imported at the top
-				 * of this file.
-				 */}
-				{data.map((item, index) => (
-					<ListItem key={index} name={item.name} />
-				))}
+				{data &&
+					data.map((item, index) => <ListItem key={index} name={item.name} />)}
 			</ul>
 		</>
 	);
