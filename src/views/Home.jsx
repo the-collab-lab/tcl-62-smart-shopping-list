7c5aa8cd2126e7c4266bf706e9c4371673e9d0b6
Magda Slifierz <m.slifierz@gmail.com>
2023-08-24 16:40:24 +0200

    new changes

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,20 +4,28 @@ import { generateToken } from '@the-collab-lab/shopping-list-utils';
 import React, { useState } from 'react';
 
 export function Home() {
+	// useNavigate hook is needed to navigate to different routes within the app
 	let navigate = useNavigate();
 
-	const { setListToken } = useState('');
+	// initialize a listToken using useState hook
+	//listToken holds the value of a token, and setListToken is the function to update its value.
+	const [listToken, setListToken] = useState('');
 
+	//declered a funtion
 	const createNewToken = () => {
+		// generating a new token
 		const newToken = generateToken();
+		// validation if not token stored in database
 		if (localStorage.getItem('token') === null) {
+			// new token is stored
 			localStorage.setItem('token', newToken);
-			//save the token using setListToken function
+			//save the token using setListToken function asign to listToken
 			setListToken(newToken);
 		}
+		//navigate to the list
 		navigate('/list');
 	};
-	//this is redirect to list if we have a token
+	//id token is stored in localStorage then navigate to list
 	if (localStorage.getItem('token')) {
 		return navigate('/list');
 	}
