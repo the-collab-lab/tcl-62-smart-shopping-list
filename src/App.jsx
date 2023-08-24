e2309b0f6ed0c6717e250be6dc5d9dac81781f56
Magda Slifierz <m.slifierz@gmail.com>
2023-08-24 22:30:16 +0200

    changes token

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
+
+import { generateToken } from '@the-collab-lab/shopping-list-utils';
 
 import { AddItem, Home, Layout, List } from './views';
 
@@ -18,6 +25,7 @@ export function App() {
 	 * have tokens), and use `setListToken` when we allow a user
 	 * to create and join a new list.
 	 */
+
 	const [listToken, setListToken] = useStateWithStorage(
 		'tcl-shopping-list-token',
 		null,
@@ -29,12 +37,33 @@ export function App() {
 	 */
 	const data = useShoppingListData(listToken);
 
+	//This function creates a new token by invoking the 'generateToken' function and returning the generated token.
+	const createNewToken = () => {
+		const newToken = generateToken();
+		return newToken;
+	};
+
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path="/list" element={<List data={data} />} />
+					<Route
+						index
+						element={
+							listToken ? (
+								<Navigate to="/list" />
+							) : (
+								<Home
+									createNewToken={createNewToken}
+									setListToken={setListToken}
+								/>
+							)
+						}
+					/>
+					<Route
+						path="/list"
+						element={listToken ? <List data={data} /> : <Navigate to="/" />}
+					/>
 					<Route path="/add-item" element={<AddItem />} />
 				</Route>
 			</Routes>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,38 +3,23 @@ import { useNavigate } from 'react-router-dom';
 import { generateToken } from '@the-collab-lab/shopping-list-utils';
 import React, { useState } from 'react';
 
-export function Home() {
-	// useNavigate hook is needed to navigate to different routes within the app
+export function Home({ createNewToken, setListToken }) {
 	let navigate = useNavigate();
 
-	// initialize a listToken using useState hook
-	//listToken holds the value of a token, and setListToken is the function to update its value.
-	const [listToken, setListToken] = useState('');
-
-	//declered a funtion
-	const createNewToken = () => {
-		// generating a new token
-		const newToken = generateToken();
-		// validation if not token stored in database
-		if (localStorage.getItem('token') === null) {
-			// new token is stored
-			localStorage.setItem('token', newToken);
-			//save the token using setListToken function asign to listToken
-			setListToken(newToken);
-		}
+	const handleClick = () => {
+		const newToken = createNewToken();
+		//save the token using setListToken function asign to listToken
+		setListToken(newToken);
 		//navigate to the list
 		navigate('/list');
 	};
-	//id token is stored in localStorage then navigate to list
-	if (localStorage.getItem('token')) {
-		return navigate('/list');
-	}
+
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
-			<button onClick={createNewToken}> Create a new list </button>
+			<button onClick={handleClick}> Create a new list </button>
 		</div>
 	);
 }
