7819a9311d57f875c66fc04ad44f17f8158ea4bf
Hans Schroeder <hajschroeder@gmail.com>
2023-08-29 15:26:20 -0700

    include search filter and button to clear search. Working

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,14 +1,51 @@
+import { useState, useEffect } from 'react';
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	// state for input
+	const [input, setInput] = useState('');
+	const [searchData, setSearchData] = useState(data);
+
+	// Handle event
+	const handleInputChange = (e) => {
+		setInput(e.target.value);
+	};
+
+	const handleInputClear = () => {
+		setInput('');
+	};
+
+	useEffect(() => {
+		const searchRegex = new RegExp(input, 'i');
+		const filteredData = data.filter((item) => searchRegex.test(item.name));
+		setSearchData(filteredData);
+	}, [data, input]);
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+
+			<label htmlFor="input">
+				<input
+					type="text"
+					id="input"
+					value={input}
+					onChange={handleInputChange}
+				/>{' '}
+				{/* need to add onChange={} */}
+			</label>
+			{input.length > 0 ? (
+				<button onClick={handleInputClear} value={input}>
+					x
+				</button>
+			) : null}
 			<ul>
-				{data &&
-					data.map((item, index) => <ListItem key={index} name={item.name} />)}
+				{searchData &&
+					searchData.map((item, index) => (
+						<ListItem key={index} name={item.name} />
+					))}
 			</ul>
 		</>
 	);
