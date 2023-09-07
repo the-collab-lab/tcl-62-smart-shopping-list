ce818957593e9f3f016e134eabe2e997c150f4d6
Hans Schroeder <hajschroeder@gmail.com>
2023-09-06 22:41:03 -0700

    include bug fix, clean up comments

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -11,51 +11,36 @@ export function ListItem({ name, listToken, dateLastPurchased }) {
 
 	// EVENT HANDLER
 	const handleCheck = async (e) => {
-		// added this variable to correctly store the value of the state
 		const currentIsChecked = !isChecked;
 		setIsChecked(currentIsChecked);
 		await updateItem(listToken, e.target.name);
-		// added this line and the useEffect beneath it to store the state of the checkbox in the local storage (it was resetting upon page refreshes before)
 		localStorage.setItem(
 			`${name}=isChecked`,
 			currentIsChecked ? 'true' : 'false',
 		);
 	};
-	// maintains the state of the checkbox despite refreshes
+
 	useEffect(() => {
-		const storedCheck = localStorage.getItem(`isChecked-${name}`);
+		const storedCheck = localStorage.getItem(`${name}=isChecked`);
 		if (storedCheck !== null) {
 			setIsChecked(storedCheck === 'true');
 		}
 	}, [name]);
-	// useEffect to set an interval of time for the checkbox to uncheck itself. I read the MDN documentation you sent on setTimeout and it was very helpful (ty), that doc recommended using setInterval for our purposes
-	useEffect(() => {
-		//declare variable for timer
-		let unCheckTimer;
 
-		// check state of checkbox
+	useEffect(() => {
+		let unCheckTimer;
 		if (isChecked) {
 			unCheckTimer = setInterval(() => {
-				// state checkbox should be after desired interval
 				setIsChecked(false);
-				// desired interval, presently set to 2 seconds for testing puposes
+				localStorage.setItem(`${name}=isChecked`, 'false');
 			}, 2000);
 		}
-		// clears the timer so it will run again if checked after !isChecked is true again
 		return () => {
-			if (unCheckTimer) clearTimeout(unCheckTimer);
+			if (unCheckTimer) {
+				clearTimeout(unCheckTimer);
+			}
 		};
-	}, [isChecked]);
-	// useEffect(() => {
-	// 	const currentTime = new Date().getTime();
-	// 	const lastPurchasedTime = new Date(dateLastPurchased).getTime();
-	// 	const timeDifference = currentTime - lastPurchasedTime;
-	// 	const twentyFourHours = 24 * 60 * 60 * 1000;
-	// 	if (timeDifference < twentyFourHours) {
-	// 		setIsChecked(false)
-	// 	}
-
-	// })
+	}, [isChecked, name]);
 
 	return (
 		<li className="ListItem">
