b18092fd5c2cce0103eca85aa3248bb88cf9563a
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-09-12 15:34:57 -0700

    Creates a helper function to set the daysSinceLastTransaction variable which will be passed to calculateEstimate as an argument

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -15,11 +15,25 @@ export function ListItem({
 	dateCreated,
 	dateNextPurchased,
 }) {
-	const currentDate = Date.now();
+	const now = Date.now() / 1000; //current time in seconds
 	// SET STATES
 	const [isChecked, setIsChecked] = useState(
-		Date.now() / 1000 - dateLastPurchased?.seconds < 60 * 60 * 24,
+		now - dateLastPurchased?.seconds < 60 * 60 * 24,
 	);
+
+	// Helper function to calculate days since last purchase
+	const getDaysSinceLastPurchase = (dateLastPurchased, dateCreated) => {
+		const oneDay = 24 * 60 * 60; // hours * minutes * seconds
+		const lastPurchasedDate = dateLastPurchased
+			? dateLastPurchased.seconds
+			: dateCreated.seconds; // Use the last purchased date if available, else use the created date
+
+		const daysSinceLastPurchase = Math.round(
+			Math.abs((now - lastPurchasedDate) / oneDay),
+		);
+
+		return daysSinceLastPurchase;
+	};
 
 	// EVENT HANDLER
 	const handleCheck = () => {
@@ -30,13 +44,20 @@ export function ListItem({
 					60 *
 					24,
 			);
-			console.log('date next:', dateNextPurchased);
-			console.log('date last:', dateLastPurchased);
-			console.log('between:', daysBetweenLastAndNextPurchased);
+
 			const previousEstimate =
 				dateLastPurchased === null ? 14 : daysBetweenLastAndNextPurchased;
-			const daysSinceLastTransaction = null;
+
+			const daysSinceLastTransaction = getDaysSinceLastPurchase(
+				dateLastPurchased,
+				dateCreated,
+			);
+			// console.log('date next:', dateNextPurchased);
+			// console.log('date last:', dateLastPurchased);
+			console.log('days since last transaction:', daysSinceLastTransaction);
+
 			// let estimatedDays = calculateEstimate( previousEstimate, daysSinceLastTransaction, totalPurchases);
+
 			// calculate date from estimated number of days and pass to updateItem
 			updateItem(listToken, itemId); // NEED TO PASS DATENEXTPURCHASED AS DATE, CALCULATED USING CALCULATEESTIMATE
 		}
